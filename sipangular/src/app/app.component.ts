4948352f291472eda9908a79250de8e15e2f3760
JAI <t.jaisaichand4@gmail.com>
2021-01-10 19:42:43 +0530

    ..

---
+++ sipangular/src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import * as JsSIP from 'jssip'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'sipangular';
+
+  clickedStart(){
+    var socket = new JsSIP.WebSocketInterface('wss://sip.myhost.com');
+var configuration = {
+  sockets  : [ socket ],
+  uri      : 'sip:alice@example.com',
+  password : 'superpassword'
+};
+
+var ua = new JsSIP.UA(configuration);
+
+ua.start();
+
+// Register callbacks to desired call events
+var eventHandlers = {
+  'progress': function(e) {
+    console.log('call is in progress');
+  },
+  'failed': function(e) {
+    console.log('call failed with cause: '+ e.data.cause);
+  },
+  'ended': function(e) {
+    console.log('call ended with cause: '+ e.data.cause);
+  },
+  'confirmed': function(e) {
+    console.log('call confirmed');
+  }
+};
+
+var options = {
+  'eventHandlers'    : eventHandlers,
+  'mediaConstraints' : { 'audio': true, 'video': true }
+};
+
+var session = ua.call('sip:bob@example.com', options);
+  }
+}
